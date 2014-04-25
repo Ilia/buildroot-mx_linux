c427392e0b21afeb26fd47147141f9adb2ec6cf3
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2014-04-25 15:09:56 +1000

    changed repo for wifimanager

--- package/thirdparty/xbmcaddons/nm/nm.mk
+++ package/thirdparty/xbmcaddons/nm/nm.mk
@@ -3,9 +3,9 @@
 ## Xbmc NetworkManager Addon
 #
 ###############################################################################
-NM_VERSION = f6e7e6e1f5a5d1dbed786cf36bc96bd90caed1ef
+NM_VERSION = 1883d2fc9f9823a7816ae2def15b3551a8c02fd0
 NM_SOURCE=nm-$(NM_VERSION).tar.gz
-NM_SITE = git://github.com/CoreTech-Development/script.linux.nm.git
+NM_SITE = https://github.com/Ilia/script.mxlinux.wifimanager.git
 NM_SITE_METHOD = git
 NM_INSTALL_STAGING = NO
 NM_INSTALL_TARGET = YES
