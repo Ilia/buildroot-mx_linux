9861d7d98b167e758e6896c2cabb44c019199cc4
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2014-05-25 08:34:42 +1000

    chorus fix build

--- package/thirdparty/xbmcaddons/chorus/chorus.mk
+++ package/thirdparty/xbmcaddons/chorus/chorus.mk
@@ -3,16 +3,18 @@
 ## Xbmc Chorus Web Interface Addon
 #
 ###############################################################################
-CHORUS_VERSION = 94e575ee5eb2bbaa607387a7c94ec1c95fe6d0ec
-CHORUS_SOURCE = webinterface.chorus-$(CHORUS_VERSION).tar.gz
-CHORUS_SITE = https://github.com/jez500/chorus.git
-CHORUS_SITE_METHOD = git
+CHORUS_VERSION = 0.3.9
+CHORUS_SOURCE = webinterface.chorus.$(CHORUS_VERSION).zip
+CHORUS_SITE = https://github.com/jez500/chorus/blob/master/ 
 CHORUS_INSTALL_STAGING = NO
 CHORUS_INSTALL_TARGET = YES
 
+define CHORUS_EXTRACT_CMDS
+        unzip $(DL_DIR)/$(FAVOURITES_SOURCE) -d $(@D)
+endef
+
 define CHORUS_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/xbmc/addons/webinterface.chorus
-	cp -rf $(@D)/* $(TARGET_DIR)/usr/share/xbmc/addons/webinterface.chorus/
+        cp -rf $(@D)/webinterface.chorus $(TARGET_DIR)/usr/share/xbmc/addons/
 endef
 
 $(eval $(call xbmc-addon,package/thirdparty/xbmcaddons,chorus))
