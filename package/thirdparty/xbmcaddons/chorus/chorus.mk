d48280bcf4f5753190487da5232a69837f85c1f0
Ilia Mogilevsky <ilia@mogilevsky.com.au>
2014-05-24 23:17:14 +1000

    chorus addon make fix

--- package/thirdparty/xbmcaddons/chorus/chorus.mk
+++ package/thirdparty/xbmcaddons/chorus/chorus.mk
@@ -4,7 +4,7 @@
 #
 ###############################################################################
 CHORUS_VERSION = 94e575ee5eb2bbaa607387a7c94ec1c95fe6d0ec
-CHORUS_SOURCE = webinterface.chorus-$(NM_VERSION).tar.gz
+CHORUS_SOURCE = webinterface.chorus-$(CHORUS_VERSION).tar.gz
 CHORUS_SITE = https://github.com/jez500/chorus.git
 CHORUS_SITE_METHOD = git
 CHORUS_INSTALL_STAGING = NO
